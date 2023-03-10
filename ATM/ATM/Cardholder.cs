eb9c522cccec83752a19b656e40c418ec758bf55
MatthewGray319 <matthewcharlesgray319@gmail.com>
2023-03-10 14:47:17 -0500

    ATM application created.

---
+++ ATM/ATM/Cardholder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class Cardholder
+    {
+        String cardNum;
+        int pin;
+        string firstName;
+        string lastName;
+        double balance;
+
+        public Cardholder(string cardNum, int pin, string firstName, string lastName, double balance)
+        {
+            this.cardNum = cardNum;
+            this.pin = pin;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.balance = balance;
+        }
+
+        public string getNum()
+        { return cardNum; }
+
+        public int getPin() { return pin; }
+
+        public string getfirstName()
+        { return firstName; }
+
+        public string getLastName() { return lastName; }
+
+        public double getBalance() { return balance; }
+
+        public void setNum(string newCardNum)
+        {
+            cardNum = newCardNum;
+        }
+
+        public void setPin(int newpin)
+        {
+            pin = newpin;
+        }
+
+        public void setFirstName(string newfirstName) { firstName = newfirstName; }
+
+        public void setLastName(string newlastName) { lastName = newlastName; }
+
+        public void setBalance(double newbalance) { balance = newbalance; }
+
+
+    }
+}
---
+++ ATM/ATM/Program.cs
@@ -0,0 +1,106 @@
+﻿using ATM;
+using System;
+
+    void printOptions()
+    {
+        Console.WriteLine("Please choose from one of the following options.");
+        Console.WriteLine("1. Deposit");
+        Console.WriteLine("2. Withdraw");
+        Console.WriteLine("3. Show Balance");
+        Console.WriteLine("4. Exit");
+    }
+
+    void deposit(Cardholder currentUser)
+    {
+        Console.WriteLine("How much money would you like to deposit? ");
+        double deposit = Double.Parse(Console.ReadLine());
+        currentUser.setBalance(deposit);
+        Console.WriteLine("Thank you for your money. Your new balance is " + currentUser.getBalance);
+    }
+
+    void withdraw(Cardholder currentUser)
+    {
+        Console.WriteLine("How much money would you like to withdraw? ");
+        double withdrawal = Double.Parse(Console.ReadLine());
+        if (currentUser.getBalance() > withdrawal)
+        {
+            Console.WriteLine("Insufficent Funds.");
+        }
+        else
+        {
+            currentUser.setBalance(currentUser.getBalance() - withdrawal);
+            Console.WriteLine("You're good to go! Thank you!");
+        }
+
+    }
+
+    void balance(Cardholder currentUser)
+    {
+        Console.WriteLine("Current Balance is " + currentUser.getBalance());
+    }
+
+
+List<Cardholder> cardholders= new List<Cardholder>();
+cardholders.Add(new Cardholder("1234567890123456", 1234, "Don", "Lemon", 150.31));
+cardholders.Add(new Cardholder("1234567890123457", 1234, "Millie", "Vanilli", 300.42));
+
+Console.WriteLine("Welcome to SimpleATM");
+Console.WriteLine("Please insert your debit card.");
+String debitCardNum = "";
+Cardholder currentUser;
+
+while(true)
+{
+    try
+    {
+        debitCardNum = Console.ReadLine();
+        currentUser = cardholders.FirstOrDefault(a => a.getNum() == debitCardNum);
+
+    }
+    catch 
+    {
+        Console.WriteLine("Card not recognized. Try again.");
+    }
+}
+
+Console.WriteLine("Please enter your pin. ");
+int userPin = 0;
+
+while (true)
+{
+    try
+    {
+        userPin = int.Parse(Console.ReadLine());
+        if (currentUser.getPin() == userPin) { break; }
+        else { Console.WriteLine("User PIN is not recognized. Please try again."); }
+
+    }
+    catch
+    {
+        Console.WriteLine("User PIN is not recognized. Try again.");
+    }
+}
+
+Console.WriteLine("Welcome " + currentUser.getfirstName);
+int option = 0;
+
+do
+{
+    printOptions();
+    try
+    {
+        option = int.Parse(Console.ReadLine());
+    }
+    catch
+    {
+
+    }
+    if (option == 1) { deposit(currentUser); }
+    else if (option == 2) { withdraw(currentUser); }
+    else if (option == 3) { balance(currentUser); }
+    else if (option == 4) { break; }
+    else { option = 0; }
+}
+while (option != 4);
+Console.WriteLine("Have a good day!!!");
+
